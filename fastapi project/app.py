66ee5810d4a3a7499363a9eeccd5b96e6097d28c
KotkuS <kathrin.savko@gmail.com>
2024-04-17 19:52:28 +0300

    initial commit

--- fastapi project/app.py
+++ fastapi project/app.py
@@ -2,3 +2,13 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+@app.get(path="/")
+async def index():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    from uvicorn import run
+    run(app=app)
